c36c3107a68164503a080241958d6e691360397b
Sakitha Senarathne <senarathnesakitha2@gmail.com>
2022-06-22 09:48:29 +0530

    Update Anagrams.scala

--- forcomp/src/main/scala/forcomp/Anagrams.scala
+++ forcomp/src/main/scala/forcomp/Anagrams.scala
@@ -87,8 +87,20 @@ object Anagrams extends AnagramsInterface {
    *  Note that the order of the occurrence list subsets does not matter -- the subsets
    *  in the example above could have been displayed in some other order.
    */
-
-  def combinations(occurrences: Occurrences): List[Occurrences] =
+  def combinations(occurrences: Occurrences): List[Occurrences] = {
+    occurrences match {
+      case head :: tail => {
+        val tailComb = combinations(tail)
+        val headComb = (1 to head._2).toList.map(i => List((head._1, i))) ::: List(List())
+        headComb.flatMap(x => if (x == Nil) tailComb else tailComb.map(_ ::: x) )
+      }
+      case head :: Nil =>
+        (1 to head._2).toList.map(i => List((head._1, i))) ::: List(List())
+      case Nil => List(List())
+    }
+  }
+  
+  def combinations_other(occurrences: Occurrences): List[Occurrences] =
   {
 
     val res = occurrences match {
